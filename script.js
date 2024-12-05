0230dd92b41e49a0bd9d0dc6b67be29daf43dbdb
Gustavo <gu.mottabs@gmail.com>
2024-12-05 06:45:28 -0300

    Carrossel de fotos

--- script.js
+++ script.js
@@ -1,4 +1,83 @@
-// script.js
+const mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const dns = require("dns");
+const cors = require ('cors')
+
+app.use(cors())
+"use client";
+import {useState} from "react";
+
+dns.setDefaultResultOrder("ipv4first");
+
+//configuração do mongodb
+const uri = `mongodb+srv://PIMaua:PiMaua@cluster0.mefla.mongodb.net/dbcontato`;
+const MongoClient = require("mongodb").MongoClient;
+const client = new MongoClient(uri); 
+
+mongoose.connect(`mongodb+srv://PIMaua:PiMaua@cluster0.mefla.mongodb.net/dbcontato`)
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+// Configuração do servidor
+console.log("server up & running");
+
+export default function ContactForm(){
+    const [nome, setNome] = useState("");
+    const [email, setEmail] = useState("");
+    const [mensagem, setMensagem] = useState("");
+}
+
+  app.get("/contato", (req, res) => {
+    res.json(contato)
+    })
+  
+  
+ let db;
+
+client.connect((err) => {
+    if (err) {
+        console.error("Erro ao conectar ao banco de dados:", err);
+        process.exit(1);
+    }
+    db = client.db("dbcontato");
+    console.log("Conectado ao banco de dados!");
+});
+
+app.post("/contato", function (req, resp) {
+    db.collection("contato").insertOne(
+        {
+            db_nome: req.body.nome,
+            db_email: req.body.email,
+            db_mensagem: req.body.mensagem,
+        },
+        (err) => {
+            if (err) {
+                resp.render("resposta", { resposta: "Falha ao contatar" });
+            } else {
+                db.collection("contato").find().toArray((err, items) => {
+                    if (err) {
+                        console.error(err);
+                        return resp.status(500).send("Erro ao buscar dados.");
+                    }
+                    resp.render("contato", { posts: items });
+                });
+            }
+        }
+    );
+});
+
+  
+  
+
+// Inicia o servidor na porta 3000
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
 
 function openModal(imgElement) {
     var modal = document.getElementById("imageModal");
@@ -13,40 +92,12 @@ function openModal(imgElement) {
 function closeModal() {
     var modal = document.getElementById("imageModal");
     modal.style.display = "none";
-
-    // Ajustar tamanho da fonte
-function adjustFontSize(action) {
-  const body = document.body;
-  const currentSize = parseFloat(window.getComputedStyle(body).fontSize);
-  if (action === 'increase') {
-    body.style.fontSize = (currentSize + 2) + 'px';
-  } else if (action === 'decrease') {
-    body.style.fontSize = (currentSize - 2) + 'px';
-  }
 }
 
-// Alterar contraste
-function toggleContrast() {
-  const body = document.body;
-  body.classList.toggle('high-contrast');
-}
+app.post('/contato', (req, res) => {
+    // Lógica de processamento do formulário
+    res.send('Formulário enviado com sucesso');
+});
 
-// Ocultar/Mostrar texto
-function toggleText() {
-  const paragraphs = document.querySelectorAll('p');
-  paragraphs.forEach(p => {
-    p.style.display = p.style.display === 'none' ? 'block' : 'none';
-  });
-}
 
-// Adicionar contraste alto ao CSS
-document.head.insertAdjacentHTML(
-  'beforeend',
-  `<style>
-    body.high-contrast {
-      background-color: black;
-      color: yellow;
-    }
-  </style>`
-);
-}
+app.use(express.urlencoded({ extended: true })); // Necessário para capturar dados de formulários
